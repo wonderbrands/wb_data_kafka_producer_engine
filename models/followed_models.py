176e1e0c7d4651a1ecbb3ed8948398dab2c16f80
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2026-05-23 04:21:57 +0200

    feat: create topic on model folloed record creation

--- models/followed_models.py
+++ models/followed_models.py
@@ -17,3 +17,31 @@ class FollowedModel(models.Model):
     api_like = fields.Boolean('API Like')
     schema_like = fields.Boolean('Schema Like')
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record in records:
+            record._create_kafka_topics()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if any(field in vals for field in ['api_like', 'schema_like', 'model', 'bootstrap_servers', 'use_uniques_bss']):
+            for record in self:
+                record._create_kafka_topics()
+        return res
+
+    def _create_kafka_topics(self):
+        for record in self:
+            if not record.model:
+                continue
+            
+            handler = self.env['kafka.message.handler']
+            if record.api_like:
+                topic_name = f"{record.model.model}-_-api_like"
+                handler._ensure_topic_exists(topic_name, model_info=record)
+            
+            if record.schema_like:
+                topic_name = f"{record.model.model}-_-schema_like"
+                handler._ensure_topic_exists(topic_name, model_info=record)
+
